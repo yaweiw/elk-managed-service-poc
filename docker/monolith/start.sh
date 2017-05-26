4e4aafd334d44c6a7a74967c275c66fbe6c5959a
yawei wang <yaweiwang@yaweis-MBP.guest.corp.microsoft.com>
2017-05-26 13:31:29 +0800

    factor out start.sh

--- docker/monolith/start.sh
+++ docker/monolith/start.sh
@@ -1,1 +1,22 @@
-aa
+#!/bin/bash
+#
+# Perform init for ELK server.
+# start.sh
+sudo service elasticsearch restart
+sudo service kibana restart
+sudo service nginx restart
+sudo service logstash restart
+
+# Load Kibana Dashboard
+apt-get -y install curl
+cd ~
+curl -L -O https://download.elastic.co/beats/dashboards/beats-dashboards-1.1.0.zip 
+apt-get -y install unzip 
+unzip beats-dashboards-*.zip 
+cd beats-dashboards-* 
+./load.sh 
+
+# Load Filebeat index template
+cd ~ 
+curl -O https://gist.githubusercontent.com/thisismitch/3429023e8438cc25b86c/raw/d8c479e2a1adcea8b1fe86570e42abab0f10f364/filebeat-index-template.json
+curl -XPUT 'http://localhost:9200/_template/filebeat?pretty' -d@filebeat-index-template.json
