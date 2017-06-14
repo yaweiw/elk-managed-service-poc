9e2ae408dbbd8601e1f96c1163209766b8b509b8
Zhiyuan Zheng <zhzy0077@hotmail.com>
2017-06-14 14:02:53 +0000

    startup.sh works

--- docker/push_images.sh
+++ docker/push_images.sh
@@ -1,14 +1,15 @@
-registry-server=elkimages.azurecr.io
-registry-username=elkimages
-registry-password=fZf=wtlP7E4JfjUNyrllZa4BHkKlxSii
+#! /bin/bash
+registry_server=elkimages.azurecr.io
+registry_username=elkimages
+registry_password='fZf=wtlP7E4JfjUNyrllZa4BHkKlxSii'
 
-docker login --username ${registry-username} --password ${registry-password} ${registry-server}
+docker login --username ${registry_username} --password ${registry_password} ${registry_server}
 
-docker build -t ${registry-server}/es:1.0.0 ./elasticsearch
-docker push ${registry-server}/es:1.0.0
-docker build -t ${registry-server}/kibana:1.0.0 ./kibana
-docker push ${registry-server}/kibana:1.0.0
-docker build -t ${registry-server}/logstash:1.0.0 ./logstash
-docker push ${registry-server}/logstash:1.0.0
-docker build -t ${registry-server}/filebeat:1.0.0 ./filebeat
-docker push ${registry-server}/filebeat:1.0.0
+docker build -t ${registry_server}/elasticsearch:1.0.0 ./elasticsearch
+docker push ${registry_server}/elasticsearch:1.0.0
+docker build -t ${registry_server}/kibana:1.0.0 ./kibana
+docker push ${registry_server}/kibana:1.0.0
+docker build -t ${registry_server}/logstash:1.0.0 ./logstash
+docker push ${registry_server}/logstash:1.0.0
+docker build -t ${registry_server}/filebeat:1.0.0 ./filebeat
+docker push ${registry_server}/filebeat:1.0.0
--- helm-elk/startup.sh
+++ helm-elk/startup.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 helm init
 
 # create namespace
@@ -6,31 +8,35 @@ namespace=elk-cluster-ns
 helm install --set common.namespace=${namespace} ns
 
 # create secret
-registry-name=azure-registry
-registry-server=elkimages.azurecr.io
-registry-username=elkimages
-registry-password=fZf=wtlP7E4JfjUNyrllZa4BHkKlxSii
-registry-email=t-zhzhe@microsoft.com
+registry_name=azure-registry
+registry_server=elkimages.azurecr.io
+registry_username=elkimages
+registry_password=fZf=wtlP7E4JfjUNyrllZa4BHkKlxSii
+registry_email=t-zhzhe@microsoft.com
 
-kubectl create secret docker-registry ${registry-name} \
---docker-server=${registry-server} \
---docker-username=${registry-username} \
---docker-password=${registry-password} \
---docker-email=${registry-email}
+kubectl --namespace=${namespace} create secret docker-registry ${registry_name} \
+--docker-server=${registry_server} \
+--docker-username=${registry_username} \
+--docker-password=${registry_password} \
+--docker-email=${registry_email}
 
 # create Elastcisearch
-helm install --set common.namespace=${namespace}\
---set image.repository=${registry-server}/es\
---set imagePullSecrets.name=${registry-name} es
+data_storageAccount=elks
+data_location=southeastasia
+helm install --set common.namespace=${namespace} \
+--set data.storageAccount=${data_storageAccount} \
+--set data.location=${data_location} \
+--set image.repository=${registry_server}/es \
+--set imagePullSecrets.name=${registry_name} ./es
 
 # create Kibana
-helm install --set common.namespace=${namespace}\
---set image.repository=${registry-server}/kibana\
---set imagePullSecrets.name=${registry-name}\
---set replicaCount=3 kibana
+helm install --set common.namespace=${namespace} \
+--set image.repository=${registry_server}/kibana \
+--set imagePullSecrets.name=${registry_name} \
+--set replicaCount=3 ./kibana
 
 # create Logstash
-helm install --set common.namespace=${namespace}\
---set image.repository=${registry-server}/logstash\
---set imagePullSecrets.name=${registry-name}\
---set replicaCount=3 logstash
+helm install --set common.namespace=${namespace} \
+--set image.repository=${registry_server}/logstash \
+--set imagePullSecrets.name=${registry_name} \
+--set replicaCount=3 ./logstash
